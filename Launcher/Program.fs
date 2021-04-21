f5837298a1de3ecdf8e7aea51cdd023650450745
Newrite <newrite.work@gmail.com>
2021-04-21 19:56:39 +0700

    add server, update launcher

--- Launcher/Program.fs
+++ Launcher/Program.fs
@@ -1,36 +1,195 @@
 ﻿open System.IO
-open System.Security.Cryptography
 open System.Collections.Generic
+open System.Security.Cryptography
+open System.Net
+open FSharp.Data
+open FSharp.Json
+open System.Text.Json
+open System.Threading.Tasks
+open Microsoft.VisualBasic
+
+let serializeToFile (d: Dictionary<string, string>) fileName =
+    let opt = JsonSerializerOptions(WriteIndented = true)
+    use writer = File.Open(fileName, FileMode.Create) |> StreamWriter
+    JsonSerializer.Serialize(d, opt) |> writer.Write
+    writer.Flush()
+
+let deserializeFromFile fileName =
+    File.ReadAllText(fileName)
+    |>JsonSerializer.Deserialize<Dictionary<string, string>>
+
+let [<Literal>] path = @"G:\testpapka"
+
+module Downloads =
+    
+    type Respons = {
+        info: string
+        link: string
+        status: bool
+    }
+    
+    let [<Literal>] private serverUrl = @"http://localhost:3030"
+    
+    let [<Literal>] private dropboxUrl = serverUrl + "/dropbox"
+    let [<Literal>] private yandexUrl = serverUrl + "/yandex"
+    let [<Literal>] private modmapUrl = serverUrl + "/modmap"
+    let [<Literal>] private tokenServer = "Bearer OMEGALULISPOWER!"
+    
+    let private headers () =
+        [ "Accept", "application/json"
+          "Authorization", tokenServer ]
+        
+    let private deserializeRespons<'a> respons =
+    
+        match respons with
+        | Ok (ok) ->
+            try
+    
+                Json.deserialize<'a> ok |> Ok
+            with :? JsonDeserializationError as eX ->
+    
+                Error
+                <| sprintf "deserializeRespons<'a> JsonDeserializationError Err:%s P: %A" eX.Message respons
+        | Error (err) ->
+    
+            Error err
+        
+    let private getYandexLink pathToFile =
+
+        try
+            let url = yandexUrl
+
+            Http.RequestString(url, httpMethod = HttpMethod.Get, query = [ "path", pathToFile ], headers = headers ())
+            |> Ok
+        with :? WebException as eX ->
+
+            Error
+            <| sprintf "%s" eX.Message
+
+    let private getDropboxLink pathToFile =
+
+        try
+            let url = dropboxUrl
+
+            Http.RequestString(url, httpMethod = HttpMethod.Get, query = [ "path", pathToFile ], headers = headers ())
+            |> Ok
+        with :? WebException as eX ->
+
+            Error
+            <| sprintf "%s" eX.Message
+            
+    let getModmapFromServer() =
+        let deserialize (text: string) =
+            text
+            |>JsonSerializer.Deserialize<Dictionary<string, string>>
+            
+        try
+            let url = modmapUrl
+
+            Http.RequestString(url, httpMethod = HttpMethod.Get, query = ["version", "release"], headers = headers ())
+            |> deserialize |> Ok
+        with :? WebException as eX ->
+
+            Error
+            <| sprintf "%s" eX.Message
+            
+    let convert (byteArray: byte[])=
+            byteArray
+            |>Array.fold (fun acc elem -> acc + elem.ToString()) ""
+            
+    let private downloadFile (url: string) (toPath: string) (sumSHA: string) =
+        use sha = new SHA1Managed()
+        let pathWithoutFile =
+            let i = toPath.LastIndexOf('\\')
+            toPath.Substring(0, i)
+        Directory.CreateDirectory(pathWithoutFile) |> printfn "%A"
+        use wc = new WebClient()
+        wc.DownloadFile(System.Uri(url), toPath+".temp")
+        File.Delete(toPath)
+        FileSystem.Rename(toPath+".temp", toPath)
+        let newSumSHA = convert <| sha.ComputeHash(File.ReadAllBytes(toPath))
+        if newSumSHA <> sumSHA then printfn "ALARM bad download"
+        
+    let startDownload pathToFile fullFilePath summSHA =
+        getYandexLink pathToFile
+        |>deserializeRespons<Respons>
+        |>function
+        | Ok (resp) ->
+            if resp.status then
+                downloadFile resp.link fullFilePath summSHA
+                Ok "Download successful"
+            else Error "Status not ok"
+        | Error (err) -> Error err
 
 
 
-let EnumerateDirectoryFilesInfo root =
-    let rec traverse (d: DirectoryInfo) =
+//Downloads.getModmapFromServer()
+//|>function
+//|Ok(dict) -> for KeyValue(k, v) in dict do
+//        printfn "key = %s value = %s" k v
+//|Error(err) -> printfn "%s" err
+
+let files sPath =
+    let rec filesUnder (basePath: string) =
         seq {
-            for f in d.GetFiles() do
-                yield f
-
-            for dd in d.GetDirectories() do
-                yield! traverse dd
+            //printfn "%s" basePath
+            if basePath.Contains("mods\\!PS") then
+                yield! Directory.GetFiles(basePath)
+            for subDir in Directory.GetDirectories(basePath) do
+                yield! filesUnder subDir
         }
+    filesUnder sPath
+        
+let seqHASH sPath =
+    let dict = Dictionary<string, string>()
+    use sha = new SHA1Managed()
+    files sPath
+    |> Seq.map (fun x -> (x, File.ReadAllBytes(x)))
+    |> Seq.iter (fun (x, y) -> dict.Add(x.Replace(sPath, "").Replace('\\','/'), (Downloads.convert <| sha.ComputeHash(y))))
+    dict
 
-    traverse (DirectoryInfo(root))
+//serializeToFile (seqHASH path) "modmap.json"
 
-
-let filesInfo =
-    EnumerateDirectoryFilesInfo(Directory.GetCurrentDirectory().ToString())
-
-let seqHASH =
-    filesInfo
-    |> Seq.map (fun x -> File.ReadAllBytes(x.ToString()))
-    |> Seq.map (fun x -> SHA256Managed().ComputeHash(x))
-
-let dict =
-    let d = Dictionary<FileInfo, string>()
-    Seq.iter2 (fun (x: byte []) y -> d.Add(y, Array.fold (fun x y -> x + y.ToString()) "" x)) seqHASH filesInfo
-    d
-
-for key in dict do
-    printf $"{key}\n"
-
-System.Console.ReadKey() |> ignore
+let compression (mapServer: Dictionary<string, string>) (mapClient: Dictionary<string, string>) =
+    
+    let fullPath (sKey: string) = path + sKey.Replace('/', '\\')
+    
+    let checkToDownload =
+        let downloadSeq = [
+            for KeyValue(k, v) in mapServer do
+                match mapClient.ContainsKey(k) with
+                | true ->
+                    //printfn "Full path = %s key = %s value1 = %s value2 = %s" fullPath k v mapClient.[k]
+                    let check = v = mapClient.[k]
+                    if check then printfn "Все в порядке" else
+                        printfn "Сумма файлов несовпадает. Вызываем асинхронную функцию на скачивание в директорию %s"
+                        <| fullPath k
+                        yield (k, v)
+                | false ->
+                    printfn "Такого файла нет. Добавляем в пул на скачивание в директорию %s"
+                    <| fullPath k
+                    yield (k, v)
+                    ]
+        let parallelOption = ParallelOptions(MaxDegreeOfParallelism = 7)
+        printfn "Всего файлов надо скачать: %d" downloadSeq.Length
+        Parallel.ForEach(downloadSeq, parallelOption, (fun (file, sum) ->
+            Downloads.startDownload file (fullPath file) sum
+            |>function
+            | Ok(resp) -> printfn "%s" resp
+            | Error (err) -> printfn "%s" err))
+                
+    let checkToDelete =
+        for k in mapClient.Keys do
+            match mapServer.ContainsKey(k) with
+            | true -> printfn "Все хорошо, такой файл есть в мапе сервера."
+            | false -> printfn "Такого файла в мапе сервера нет, запускаем функцию удаления в путь %s"
+                       <| fullPath k
+                       File.Delete(fullPath k)
+                       
+    checkToDownload |> printfn "%A"
+    checkToDelete
+            
+Downloads.getModmapFromServer()
+|>function
+|Ok(dict) -> compression dict (seqHASH path)
+|Error(err) -> printfn "%s" err
---
+++ Server/Program.fs
@@ -0,0 +1,147 @@
+﻿open Saturn
+open Giraffe
+open Microsoft
+open System.IO
+open System.Collections.Generic
+open System.Text.Json
+open Dropbox
+open YandexDisk
+open Microsoft.Extensions.Logging
+
+module Constants =
+    let [<Literal>] qPathKey = "path"
+    let [<Literal>] qModmapKey = "version"
+    let [<Literal>] qModmapValueRelease = "release"
+    let [<Literal>] fileNameModMapRelease = "E:\Programming\F#\LaunchServer\modmap.json"
+    let [<Literal>] serverStatus = true
+    let [<Literal>] information = "Already ok"
+    let [<Literal>] Authorization = "Authorization"
+    let [<Literal>] Token = "Bearer OMEGALULISPOWER!"
+    let [<Literal>] tokendbx = "t4ld3cJMM9IAAAAAAAAAAXuuGeo2mLcMZxaYXjl7oaIg_up7yVTeJmp23ekpxERM"
+    let [<Literal>] tokendisk = "AQAAAABT8kstAAcN-S_wbWiH8kcMgqgu20WWdo4"
+    
+    let modmap =
+        let deserializeFromFile fileName =
+            File.ReadAllText(fileName)
+            |>JsonSerializer.Deserialize<Dictionary<string, string>>
+        deserializeFromFile fileNameModMapRelease
+
+module DiskRequests =
+    let private dropboxDiskInstance = new Api.DropboxClient(Constants.tokendbx)
+    
+    let private yandexDiskInstance = new Client.Http.DiskHttpApi(Constants.tokendisk)
+    
+    let private memoize(f: 'a -> 'b) (filter: Dictionary<'a, 'b> -> unit) =
+        let dict = new Dictionary<'a, 'b>()
+        
+        let memoizedFunc (input: 'a) =
+            filter dict
+            match dict.TryGetValue(input) with
+            |true, x -> x
+            |false, _ ->
+                let answer = f input
+                dict.Add(input, answer)
+                answer
+                
+        memoizedFunc
+    
+    let private _requestToYandexDisk path =
+        try
+            let file = yandexDiskInstance.Files.GetDownloadLinkAsync(path)
+            file.Wait()
+            {|Status = true ;Link = file.Result.Href; Info = "Ok"|}
+        with _ as eX -> {|Status = false; Link = ""; Info = "Can't get file from yandex"|}
+        
+    let private _requestToDropboxDisk path =
+        try
+            let file = dropboxDiskInstance.Files.GetTemporaryLinkAsync("/"+path)
+            file.Wait()
+            {|Status = true ;Link = file.Result.Link; Info = "Ok"|}
+        with _ as eX -> {|Status = false; Link = ""; Info = "Can't get file from dropbox"|}
+        
+    let requestToDisk next (ctx: AspNetCore.Http.HttpContext) request =
+        match ctx.Request.Query.ContainsKey(Constants.qPathKey) with
+        |true ->
+            (ctx.Request.Query.[Constants.qPathKey].ToString()
+             |> request
+             |> json) next ctx
+        |false -> (RequestErrors.METHOD_NOT_ALLOWED "no query") next ctx
+        
+    let requestToYandexDisk = memoize _requestToYandexDisk (fun dict -> printfn "%A" dict)
+    let requestToDropboxDisk = memoize _requestToDropboxDisk (fun dict -> printfn "%A" dict)
+
+type ResponsStatus =
+    { StatusLine: bool
+      Information: string }
+    
+let checkHeader (headers: AspNetCore.Http.IHeaderDictionary) headerToCheck (valueToFind: string) =
+    if headers.ContainsKey(headerToCheck)  then
+        headers.[headerToCheck].ToString().Contains(valueToFind)
+    else
+        false
+
+let getResponsStatus = { StatusLine = Constants.serverStatus; Information = Constants.information }
+
+module Pipes =
+    let Auth = pipeline {
+        plug (fun next ctx ->
+            match checkHeader ctx.Request.Headers Constants.Authorization Constants.Token with
+            | true -> next ctx
+            | false -> (RequestErrors.UNAUTHORIZED "Bearer" "Server" "Authorization error.") next ctx)
+    }
+    
+    let AcceptJson = pipeline {
+        plug acceptJson
+    } 
+        
+        
+module Routers =
+    let Server = router {
+        get "/status" (json getResponsStatus )
+    }
+    
+    let Modmap = router {
+        get "/modmap" (fun next ctx ->
+            match ctx.Request.Query.ContainsKey(Constants.qModmapKey) with
+            |true when ctx.Request.Query.[Constants.qModmapKey].ToString().Contains(Constants.qModmapValueRelease) ->
+                (json <| Constants.modmap) next ctx
+            |true -> (RequestErrors.BAD_REQUEST "Wrong request.") next ctx
+            |false -> (RequestErrors.METHOD_NOT_ALLOWED "no query") next ctx
+            |_ -> (ServerErrors.NOT_IMPLEMENTED "What the fuck.") next ctx)
+    }
+    
+    
+    let YandexDisk = router {
+        get "/yandex" (fun next ctx -> DiskRequests.requestToDisk next ctx DiskRequests.requestToYandexDisk)
+    }
+    
+    let DropBoxDisk = router {
+        get "/dropbox" (fun next ctx -> DiskRequests.requestToDisk next ctx DiskRequests.requestToDropboxDisk)
+    }
+    
+    let Disk = router {
+        //pipe_through pAcceptJson
+        forward "" YandexDisk
+        forward "" DropBoxDisk
+    }
+
+let mainRouter = router {
+    not_found_handler (text "Error: 404 wrong page")
+    //pipe_through pAuth
+    forward "" Routers.Server
+    forward "" Routers.Modmap
+    forward "" Routers.Disk
+}
+
+let configureLogging (logging: ILoggingBuilder) =
+    logging.SetMinimumLevel(LogLevel.None) |> ignore
+
+let app = application {
+    url "http://0.0.0.0:3030"
+    use_router mainRouter
+    memory_cache
+    use_gzip
+    //logging configureLogging
+}
+
+run app
